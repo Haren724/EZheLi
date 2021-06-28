21602a7ad7303fd589ad485a592ed333da84b002
Chikun Chen <human01d@icloud.com>
2021-06-28 09:48:17 +0800

    Initial Commit

---
+++ Shared/EZheLiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EZheLiApp.swift
+//  Shared
+//
+//  Created by 陈驰坤 on 2021/6/28.
+//
+
+import SwiftUI
+
+@main
+struct EZheLiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
